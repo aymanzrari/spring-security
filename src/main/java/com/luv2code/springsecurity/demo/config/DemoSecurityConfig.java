893758efaf80a8fa2d6050ff0d081b858e7aa939
Ayman Zrari <Ayman Zrari@DESKTOP-BM9J5CU>
2020-10-06 15:40:28 +0100

    add connection with JDBC

--- src/main/java/com/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
+++ src/main/java/com/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
@@ -1,5 +1,8 @@
 package com.luv2code.springsecurity.demo.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,25 +15,19 @@ import org.springframework.security.core.userdetails.User.UserBuilder;
 @EnableWebSecurity
 public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	private DataSource securityDataSource;
+	
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-
-		// add our users for in memory authentification
-		
-		UserBuilder users=User.withDefaultPasswordEncoder();
-		
-		auth.inMemoryAuthentication()
-			.withUser(users.username("ayman").password("ayman").roles("Employee"))
-			.withUser(users.username("oumaima").password("oumaima").roles("Employee","MANAGER"))
-			.withUser(users.username("admin").password("admin").roles("Employee","MANAGER","ADMIN"));
-		
+		auth.jdbcAuthentication().dataSource(securityDataSource);		
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		
 		http.authorizeRequests()
-				.antMatchers("/").hasRole("Employee")
+				.antMatchers("/").hasRole("EMPLOYEE")
 				.antMatchers("/leaders/**").hasRole("MANAGER")
 				.antMatchers("/systems/**").hasRole("ADMIN")
 			.and()
